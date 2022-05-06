ebf771ca0bd94a80d41432b16f8d9d055b5fcfc6
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-06 13:33:50 +0000

    [Add]スライドショー追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "76bbf3518c544bbda6c4158b39c4e12c.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
+
